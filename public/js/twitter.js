325e92491c19e388b9aa41986fcf9001c3343787
Elliot Zoerner <eliot.zoerner@gmail.com>
2017-06-12 11:05:20 +0200

    sign in protection

--- public/js/twitter.js
+++ public/js/twitter.js
@@ -1,6 +1,6 @@
 
 $(function() {
 	$("#sign-in-with-twitter").on("click", function() {
-		window.location.href = window.location.href + "request-token";
+		window.location.href = "https://ez-vote.herokuapp.com/request-token";
 	});
 });
