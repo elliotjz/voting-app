ff0b875e3972a336036417f89d0f0ce5a877c95b
Elliot Zoerner <eliot.zoerner@gmail.com>
2017-06-12 11:11:40 +0200

    added detail to ajax request

--- public/js/signed-in.js
+++ public/js/signed-in.js
@@ -1,5 +1,18 @@
 
 $(function() {
+	$.ajax({
+		url: "https://ez-vote.herokuapp.com/access-token" + location.search,
+		type: 'GET',
+		success: function(user) {
+			$("#personal-message").html("Hello " + user.name);
+	        console.log(user);
+	        console.log(user.name);
+		},
+		error: function(err) {
+			window.location.href = "https://ez-vote.herokuapp.com/sign-in-fail";
+		}
+	});
+	/*
     $.get("https://ez-vote.herokuapp.com/access-token" + location.search).done(function(user) {
     	if (user) {
     		$("#personal-message").html("Hello " + user.name);
@@ -10,4 +23,5 @@ $(function() {
     	}
         
     });
+    */
 });
